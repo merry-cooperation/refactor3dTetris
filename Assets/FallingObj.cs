579b18f502a471e448dc7de1218f8e22fb242bd7
Oleg Abramov <oleg.abramov100500@yandex.ru>
2018-06-01 23:22:44 +0700

    experimental scene

---
+++ Assets/FallingObj.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObj : MonoBehaviour
+{
+    public delegate void StopFallHandler(Vector3 pos);
+    public static event StopFallHandler StopFallEvent;
+
+    void Start()
+    {
+        GameManager.instance.MoveXEvent += new GameManager.MoveXHandler(HandleMoveX);
+        GameManager.instance.MoveZEvent += new GameManager.MoveZHandler(HandleMoveZ);
+        GameManager.instance.GameTickEvent += new GameManager.GameTickHandler(HandleGameTick);
+    }
+
+    private void HandleMoveX(bool right)
+    {
+        if (right)
+        {
+            transform.Translate(1, 0, 0);
+        }
+        else
+        {
+            transform.Translate(-1, 0, 0);
+        }
+    }
+
+    private void HandleMoveZ(bool up)
+    {
+        if (up)
+        {
+            transform.Translate(0, 0, 1);
+        }
+        else
+        {
+            transform.Translate(0, 0, -1);
+        }
+    }
+
+    void HandleGameTick()
+    {
+        if (transform.position.y >= 1)
+        {
+            transform.Translate(0, -1, 0);
+
+        }
+        else
+        {
+
+            if (StopFallEvent != null)
+            {
+                StopFallEvent(transform.position);
+            }
+
+            GameManager.instance.MoveXEvent -= HandleMoveX;
+            GameManager.instance.MoveZEvent -= HandleMoveZ;
+            GameManager.instance.GameTickEvent -= HandleGameTick;
+            Destroy(this);
+        }
+
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    public delegate void GameTickHandler();
+    public event GameTickHandler GameTickEvent;
+    public delegate void MoveXHandler(bool right);
+    public event MoveXHandler MoveXEvent;
+    public delegate void MoveZHandler(bool up);
+    public event MoveZHandler MoveZEvent;
+
+    public GameObject cubePrefab;
+
+    public float period;
+    private float progress;
+
+    public enum Piece { Cube };
+
+    private enum Cell { Space, Piece };
+    private Cell[,,] glass;
+    private const int SIZEX = 5;
+    private const int SIZEY = 10;
+    private const int SIZEZ = 5;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+        glass = new Cell[SIZEX, SIZEY, SIZEZ];
+        progress = 0.0f;
+
+
+        FallingObj.StopFallEvent += new FallingObj.StopFallHandler(StopFallHandler);
+    }
+
+    public bool MovePossible(Vector3 pos)
+    {
+        int x = (int)pos.x;
+        int y = (int)pos.y;
+        int z = (int)pos.z;
+
+    }
+
+    void StopFallHandler(Vector3 pos)
+    {
+        Instantiate(cubePrefab, new Vector3(2, 9, 2), Quaternion.identity);
+        glass[(int)pos.x, (int)pos.y, (int)pos.z] = Cell.Piece;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (progress >= period)
+        {
+            progress -= period;
+            if (GameTickEvent != null)
+            {
+                GameTickEvent();
+            }
+
+        }
+        else
+        {
+            progress += Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (MoveZEvent != null)
+            {
+                MoveZEvent(true);
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (MoveZEvent != null)
+            {
+                MoveZEvent(false);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (MoveZEvent != null)
+            {
+
+                MoveXEvent(true);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (MoveZEvent != null)
+            {
+                MoveXEvent(false);
+            }
+        }
+    }
+
+}
