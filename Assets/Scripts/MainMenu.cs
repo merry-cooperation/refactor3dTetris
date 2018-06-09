1de665a9ad3f15c9b3a590f5cd6d68d6780c25ca
DESKTOP-QR7F1VD\sasha <sasha_yudin1998@mail.ru>
2018-06-09 12:16:30 +0700

    add menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    GameObject scores;
+    GameObject menu;
+
+    private void Awake()
+    {
+        scores = transform.GetChild(0).gameObject;
+        menu = transform.GetChild(1).gameObject;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            scores.SetActive(false);
+            menu.SetActive(true);
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quite()
+    {
+        Application.Quit();
+    }
+
+    public void HightScores()
+    {
+        menu.SetActive(false);
+        scores.SetActive(true);
+    }
+}
